468ee407d4196b12ee9cec37fdbae7e31b6a8d22
Jose Naylor <jose.c.naylor@gmail.com>
2017-08-14 16:54:35 -0400

    downloaded and completed brussel sprout exercise

---
+++ brussels-sprouts-fetch/react/src/containers/SproutsContainer.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import RandomSprout from '../components/RandomSprout';
+import SproutsIndex from '../components/SproutsIndex';
+
+class SproutsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipe: "",
+      recipes: []
+    }
+  }
+
+  getRandomRecipe(){
+    fetch('/api/v1/random-recipe')
+      .then(response => {
+        if (response.ok) {
+          console.log('response ok')
+          return response.json()
+        }else {
+          let error = new Error()
+          throw(error)
+        }
+      })
+      .then( body => {
+        this.setState({recipe:body, recipes: []})
+      })
+      .catch(error => console.error())
+  }
+
+  getAllRecipes(){
+    fetch('/api/v1/recipes')
+      .then(response => {
+        if (response.ok) {
+          console.log('response ok')
+          return response.json()
+        }else {
+          let error = new Error()
+          throw(error)
+        }
+      })
+      .then( body => {
+        this.setState({recipe:"", recipes: body})
+      })
+      .catch(error => console.error())
+  }
+
+  render(){
+
+    let handleRandomClick = () => {
+      this.getRandomRecipe();
+    }
+
+    let handleIndexClick = () => {
+      this.getAllRecipes();
+    }
+
+    return(
+      <div className="container">
+        <h1>Sprout Fetcher</h1>
+        <RandomSprout
+          recipe={this.state.recipe}
+          handleClick = {handleRandomClick}
+        />
+        <SproutsIndex
+          recipes={this.state.recipes}
+          handleClick={handleIndexClick}
+        />
+
+        <div className="buttons">
+          <button onClick={handleRandomClick} className="btn">Get Random Recipe</button>
+
+          <button onClick={handleIndexClick} className="btn">See All Recipes</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SproutsContainer;
